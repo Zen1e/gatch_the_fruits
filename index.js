b3d0a8e5b870bc4990506087c69246145667d06e
Zen1e <monkhdelger.edts@gmail.com>
2024-12-19 18:05:39 +0800

    bug fix

--- index.js
+++ index.js
@@ -14,23 +14,23 @@ let score = 0;
 let lives = 4;
 let highestScore = 0;
 const pointer = document.createElement("div");
-const livesCont = document.createElement("div")
-livesCont.classList.add("liveCont")
-livesCont.id = "liveCont"
-const liveIcon1 = document.createElement("div")
-const liveIcon2 = document.createElement("div")
-const liveIcon3 = document.createElement("div")
-liveIcon1.classList.add("liveIcon")
-liveIcon2.classList.add("liveIcon")
-liveIcon3.classList.add("liveIcon")
-pointer.classList.add("pointer")
-pointer.id  = "pointer"
-document.getElementById("container").appendChild(pointer)
-document.getElementById("container").appendChild(livesCont)
+const livesCont = document.createElement("div");
+livesCont.classList.add("liveCont");
+livesCont.id = "liveCont";
+const liveIcon1 = document.createElement("div");
+const liveIcon2 = document.createElement("div");
+const liveIcon3 = document.createElement("div");
+liveIcon1.classList.add("liveIcon");
+liveIcon2.classList.add("liveIcon");
+liveIcon3.classList.add("liveIcon");
+pointer.classList.add("pointer");
+pointer.id = "pointer";
+document.getElementById("container").appendChild(pointer);
+document.getElementById("container").appendChild(livesCont);
 document.getElementById("liveCont").appendChild(liveIcon1);
 document.getElementById("liveCont").appendChild(liveIcon2);
 document.getElementById("liveCont").appendChild(liveIcon3);
-document.getElementById("pointer").innerHTML = "score:"+score
+document.getElementById("pointer").innerHTML = "score:" + score;
 
 //Start
 function gameStart() {
@@ -62,11 +62,21 @@ function gameStart() {
   cherry.classList.add("hidden");
   const fruit = document.getElementsByClassName("fruit");
 
-//   let random = Math.floor(Math.random() * 950);
-//   let oldRan = random, i = 0;
-//   fruit[0].style.left = random + "px";
-let i=0, oldRan = -100;
+  const poo = document.createElement("div");
+  gameCont.appendChild(poo);
+  poo.classList.add("fruit");
+  poo.classList.add("pooImage");
+  poo.classList.add("hidden");
+  const bombPoo = document.getElementsByClassName("fruit");
+
+  //   let random = Math.floor(Math.random() * 950);
+  //   let oldRan = random, i = 0;
+  //   fruit[0].style.left = random + "px";
+  let i = 0,
+    oldRan = -100;
   setInterval(fruitFall, 3000);
+
+  setInterval(bombs, 6000);
 
   function fruitFall() {
     fruit[i].classList.remove("animate");
@@ -76,9 +86,27 @@ let i=0, oldRan = -100;
     fruit[i].classList.add("animate");
     fruit[i].classList.remove("hidden");
     fruit[i].style.left = random + "px";
-    setTimeout(score_check(oldRan), 3000)
-    oldRan = random
+    setTimeout(score_check(oldRan), 3000);
+    oldRan = random;
   }
+
+  function bombs() {
+    setTimeout(() => {
+    poo.classList.remove("animate");
+    poo.classList.add("hidden");
+    let randomPoo = Math.floor(Math.random() * 950);
+    poo.classList.add("animate");
+    poo.classList.remove("hidden");
+    poo.style.left = randomPoo + "px";
+    setTimeout(remover => {poo.classList.remove("animate"), poo.classList.add("hidden"); pooCheck(randomPoo)}, 2900)
+    }, 1500);
+}
+function pooCheck(randomPoo){
+    let leftEdge = position.left - 30;
+    let rightEdge = leftEdge + 150;
+    if (leftEdge <= randomPoo && rightEdge >= randomPoo){
+        lives = live_u(lives);
+    }}
 
   //Basket
   let modifier = 80;
@@ -113,34 +141,31 @@ let i=0, oldRan = -100;
 
   document.addEventListener("keydown", moveBasket);
 
-let t=0;
-  function score_check(oldRan){
-      let leftEdge = position.left-30;
-      let rightEdge = leftEdge+150;
-      if(leftEdge<=oldRan&&rightEdge>=oldRan){
-            score+=1;
-            console.log(score);
-            
-            document.getElementById("pointer").innerHTML = "score:"+score}
-        else{
-            lives -=1;
-            console.log(lives);
-            if (lives==2){
-                document.getElementById("liveCont").removeChild(liveIcon2);
-                
-            }else if (lives==1){
-                document.getElementById("liveCont").removeChild(liveIcon3);;
-            }else if (lives == 0){
-                document.getElementById("liveCont").removeChild(liveIcon1);
-                // gameOver();
-                alert(score)
-            }
-        }
-        }
-function lives_u(){
-    
-}
-}
+  let t = 0;
+  function score_check(oldRan) {
+    let leftEdge = position.left - 30;
+    let rightEdge = leftEdge + 150;
+    if (leftEdge <= oldRan && rightEdge >= oldRan) {
+      score += 1;
 
-
-
+      document.getElementById("pointer").innerHTML = "score:" + score;
+    } else {
+        lives = live_u(lives);
+      }
+    }
+    function live_u(lives){
+      lives-= 1;
+      console.log(lives);
+      if (lives == 2) {
+        document.getElementById("liveCont").removeChild(liveIcon2);
+      } else if (lives == 1) {
+        document.getElementById("liveCont").removeChild(liveIcon3);
+      } else if (lives == 0) {
+        document.getElementById("liveCont").removeChild(liveIcon1);
+        // gameOver();
+        alert(score);
+    }
+    return lives;
+  //   function lives_u() {}
+  }
+  }
